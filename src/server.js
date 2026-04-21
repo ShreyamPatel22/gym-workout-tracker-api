2ea24f73f59765074c200a315251ad45579ddab5
Shreyam <shreyampatel22@gmail.com>
2026-04-21 16:43:41 -0400

    feat: add swagger documentation with reusable components

--- src/server.js
+++ src/server.js
@@ -1,16 +1,23 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
+import { readFileSync } from "fs";
+import { load } from "js-yaml";
 import authRoutes from "./routes/authRoutes.js";
 import workoutRoutes from "./routes/workoutRoutes.js";
 import exerciseRoutes from "./routes/exerciseRoutes.js";
 import workoutLogRoutes from "./routes/workoutLogRoutes.js";
+
+const swaggerDoc = load(readFileSync("./swagger.yaml", "utf8"));
 
 const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/workouts", workoutRoutes);
