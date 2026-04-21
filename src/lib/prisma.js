b5d42f2e25f951a08c29a4c7cdef9fb1a27506c9
Shreyam <shreyampatel22@gmail.com>
2026-04-21 01:02:52 -0400

    fix: release prisma connection on process exit

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -4,4 +4,6 @@ import { PrismaClient } from "../generated/prisma/client.ts";
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
+process.on("beforeExit", async () => await prisma.$disconnect());
+
 export default prisma;
